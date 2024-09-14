21ca3a3b875ddbc4766652a5eb3ce4a2d8b4f0e5
code043 <procoder043@gmail.com>
2024-09-14 00:20:28 -0300

    Paginação

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import Data from "../Data.json";
+import "./index.css";
+
+function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordsPerPage = 5;
+  const lastIndex = currentPage * recordsPerPage;
+  const firstIndex = lastIndex - recordsPerPage;
+  const records = Data.slice(firstIndex, lastIndex);
+  const npage = Math.ceil(Data.length / recordsPerPage);
+  const numbers = [...Array(npage + 1).keys()].slice(1);
+  function prePage() {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+  function nextPage() {
+    if (currentPage !== npage) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+  function changeCPage(id) {
+    setCurrentPage(id);
+  }
+  return (
+    <div className="table">
+      <table style={{ width: "500px" }}>
+        <thead>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+        </thead>
+        <tbody>
+          {records.map((d, i) => (
+            <tr key={i}>
+              <td>{d.ID}</td>
+              <td>{d.name}</td>
+              <td>{d.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <nav>
+        <ul className="pagination">
+          <li className="page-item">
+            <a href="#" className="page-link" onClick={prePage}>
+              Prev
+            </a>
+          </li>
+
+          {numbers.map((n, i) => (
+            <li
+              className={`page-item ${currentPage === n ? "active" : ""}`}
+              key={i}
+            >
+              <a href="#" className="page-item" onClick={() => changeCPage(n)}>
+                {n}
+              </a>
+            </li>
+          ))}
+          <li className="page-item">
+            <a href="#" className="page-link" onClick={nextPage}>
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
